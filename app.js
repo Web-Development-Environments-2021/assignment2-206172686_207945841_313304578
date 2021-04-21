c6cca74f4026e7fefc63436e38ed7db55d515323
unknown <saritholl@gmail.com>
2021-04-21 23:35:39 +0300

    add moving food

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var context;
 var pacman_position = new Object();
+var strawberry_position = new Object();
 var board;
 var score;
 var amount_of_balls_remain;
@@ -15,8 +16,13 @@ const MIN_TIME_SECONDS = 60;
 const MIN_MONSTERS_AMOUNT = 1
 const MAX_MONSTERS_AMOUNT = 4
 const WALL = 4
+
+STRAWBERRY_POINTS_VALUE = 50
+
 const TIME_BUNOS = 201
 const PILL = 202
+const STRAWBERRY = 203
+
 const FOOD_5_POINTS = 100
 const FOOD_15_POINTS = 101
 const FOOD_25_POINTS = 102
@@ -33,7 +39,7 @@ const UP_MOVE = 3
 const DOWN_MOVE = 4
 
 const COLS = 10
-const ROWS = 10 
+const ROWS = 10
 
 
 var current_lifes = 5
@@ -44,19 +50,20 @@ var BALL_25_COLOR = "#00b33c"
 var TOTAL_TIME = 120
 var MONSTERS_AMOUNT = 2
 
-$(document).ready(function() {
+$(document).ready(function () {
 	context = canvas.getContext("2d");
 	timer = document.getElementById('timer');
 	pill = document.getElementById('pill');
+	strawberry = document.getElementById('strawberry');
 });
-  
 
-document.getElementById("startGameBtn").onclick = function(){
+
+document.getElementById("startGameBtn").onclick = function () {
 	audio.play()
 	Start();
 };
 
-document.getElementById("settingsRandomValuesBtn").onclick = function(){
+document.getElementById("settingsRandomValuesBtn").onclick = function () {
 	numOfBalls.value = getRandomInt(MIN_BALLS_AMOUNT, MAX_BALLS_AMOUNT)
 	ball5color.value = getRandomColor()
 	ball15color.value = getRandomColor()
@@ -67,24 +74,24 @@ document.getElementById("settingsRandomValuesBtn").onclick = function(){
 };
 
 function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 function getRandomColor() {
 	var letters = '0123456789ABCDEF';
 	var color = '#';
 	for (var i = 0; i < 6; i++) {
-	  color += letters[Math.floor(Math.random() * 16)];
+		color += letters[Math.floor(Math.random() * 16)];
 	}
 	return color;
-  }
+}
 
-window.addEventListener("keydown", function(e) {
-    if(["Space","ArrowUp","ArrowDown","ArrowLeft","ArrowRight"].indexOf(e.code) > -1) {
-        e.preventDefault();
-    }
+window.addEventListener("keydown", function (e) {
+	if (["Space", "ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight"].indexOf(e.code) > -1) {
+		e.preventDefault();
+	}
 }, false);
 
 function Start() {
@@ -162,13 +169,21 @@ function Start() {
 	var emptyCell = findRandomEmptyCell(board);
 	board[emptyCell[0]][emptyCell[1]] = TIME_BUNOS;
 
-	var emptyCell = findRandomEmptyCell(board);
+	emptyCell = findRandomEmptyCell(board);
 	board[emptyCell[0]][emptyCell[1]] = PILL;
+
+	emptyCell = findRandomEmptyCell(board);
+	board[emptyCell[0]][emptyCell[1]] = STRAWBERRY;
+
+	strawberry_position.i = emptyCell[0];
+	strawberry_position.j = emptyCell[1];
+	strawberry_position.prev_value = EMPTY_CELL
+	strawberry_position.is_alive = true
 
 	keyDown = -1;
 	addEventListener(
 		"keydown",
-		function(e) {
+		function (e) {
 			keyDown = e.keyCode
 		},
 		false
@@ -265,8 +280,8 @@ function Draw(move = RIGHT_MOVE) {
 
 				context.fillStyle = "white";
 				context.font = "16px Arial";
-				context.fillText("5", center.x - 5 , center.y + 5);
-			} 
+				context.fillText("5", center.x - 5, center.y + 5);
+			}
 			else if (board[i][j] == FOOD_15_POINTS) {
 				context.beginPath();
 				context.arc(center.x, center.y, 15, 0, 2 * Math.PI); // circle
@@ -274,7 +289,7 @@ function Draw(move = RIGHT_MOVE) {
 				context.fill();
 				context.fillStyle = "white";
 				context.font = "16px Arial";
-				context.fillText("15", center.x - 10 , center.y + 5);
+				context.fillText("15", center.x - 10, center.y + 5);
 			}
 			else if (board[i][j] == FOOD_25_POINTS) {
 				context.beginPath();
@@ -283,7 +298,7 @@ function Draw(move = RIGHT_MOVE) {
 				context.fill();
 				context.fillStyle = "white";
 				context.font = "16px Arial";
-				context.fillText("25", center.x - 10 , center.y + 5);
+				context.fillText("25", center.x - 10, center.y + 5);
 			} else if (board[i][j] == WALL) {
 				context.beginPath();
 				context.rect(center.x - 30, center.y - 30, 60, 60);
@@ -296,32 +311,24 @@ function Draw(move = RIGHT_MOVE) {
 			else if (board[i][j] == PILL) {
 				context.drawImage(pill, center.x - 20, center.y - 20, 35, 35);
 			}
+			else if (board[i][j] == STRAWBERRY && strawberry_position.is_alive) {
+				context.drawImage(strawberry, center.x - 20, center.y - 20, 35, 35);
+			}
 		}
 	}
 }
 
 function UpdatePosition() {
 	board[pacman_position.i][pacman_position.j] = EMPTY_CELL;
-	var x = GetKeyPressed();
-	if (x == UP_MOVE) {
-		if (pacman_position.j > 0 && board[pacman_position.i][pacman_position.j - 1] != WALL) {
-			pacman_position.j--;
-		}
-	}
-	else if (x == DOWN_MOVE) {
-		if (pacman_position.j < ROWS - 1  && board[pacman_position.i][pacman_position.j + 1] != WALL) {
-			pacman_position.j++;
-		}
-	}
-	else if (x == LEFT_MOVE) {
-		if (pacman_position.i > 0 && board[pacman_position.i - 1][pacman_position.j] != WALL) {
-			pacman_position.i--;
-		}
-	}
-	else if (x == RIGHT_MOVE) {
-		if (pacman_position.i < COLS - 1 && board[pacman_position.i + 1][pacman_position.j] != WALL) {
-			pacman_position.i++;
-		}
+	curr_move = GetKeyPressed()
+
+	movePosition(pacman_position, curr_move);
+
+	if (strawberry_position.is_alive) {
+		board[strawberry_position.i][strawberry_position.j] = strawberry_position.prev_value;
+		movePosition(strawberry_position, getRandomInt(1, 4));
+		strawberry_position.prev_value = board[strawberry_position.i][strawberry_position.j]
+		board[strawberry_position.i][strawberry_position.j] = STRAWBERRY;
 	}
 	if (board[pacman_position.i][pacman_position.j] == FOOD_5_POINTS) {
 		score += 5;
@@ -334,8 +341,11 @@ function UpdatePosition() {
 		amount_of_balls_remain--;
 	} else if (board[pacman_position.i][pacman_position.j] == TIME_BUNOS) {
 		TOTAL_TIME += TIME_BUNOS_SECONDS
-	}else if (board[pacman_position.i][pacman_position.j] == PILL) {
+	} else if (board[pacman_position.i][pacman_position.j] == PILL) {
 		current_lifes += 1
+	} else if (board[pacman_position.i][pacman_position.j] == STRAWBERRY) {
+		score += STRAWBERRY_POINTS_VALUE
+		strawberry_position.is_alive = false
 	}
 
 	board[pacman_position.i][pacman_position.j] = PACMAN;
@@ -343,21 +353,21 @@ function UpdatePosition() {
 	time_elapsed = (currentTime - start_time) / 1000;
 	time_remain = ~~(TOTAL_TIME - time_elapsed);
 	if (amount_of_balls_remain == 0) {
-		Draw(x);
+		Draw(curr_move);
 		window.clearInterval(interval);
 		window.alert("Winner!!!");
-		
-	} else if(time_remain == 0) {
-		Draw(x);
+
+	} else if (time_remain == 0) {
+		Draw(curr_move);
 		window.clearInterval(interval);
 		window.alert("You are better than " + score + " Points!");
-	} 
+	}
 	else {
-		Draw(x);
+		Draw(curr_move);
 	}
 }
 
-document.getElementById("settings_form").onsubmit = function() {
+document.getElementById("settings_form").onsubmit = function () {
 	TOTAL_FOOD_AMOUNT = numOfBalls.value
 	BALL_5_COLOR = ball5color.value
 	BALL_15_COLOR = ball15color.value
@@ -367,3 +377,26 @@ document.getElementById("settings_form").onsubmit = function() {
 
 	alert("Let's play!")
 };
+function movePosition(position, direction) {
+	if (direction == UP_MOVE) {
+		if (position.j > 0 && board[position.i][position.j - 1] != WALL) {
+			position.j--;
+		}
+	}
+	else if (direction == DOWN_MOVE) {
+		if (position.j < ROWS - 1 && board[position.i][position.j + 1] != WALL) {
+			position.j++;
+		}
+	}
+	else if (direction == LEFT_MOVE) {
+		if (position.i > 0 && board[position.i - 1][position.j] != WALL) {
+			position.i--;
+		}
+	}
+	else if (direction == RIGHT_MOVE) {
+		if (position.i < COLS - 1 && board[position.i + 1][position.j] != WALL) {
+			position.i++;
+		}
+	}
+}
+
